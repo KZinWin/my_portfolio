fb7ad24eadad8c79cebdcc69e4311c438faa1122
K-Zin <mysterioiusk82@gmail.com>
2023-06-30 00:33:46 +0630

    add warning alert

--- pages/api/email.tsx
+++ pages/api/email.tsx
@@ -6,6 +6,15 @@ export default async function handler(req : any , res: any) {
     const message = req.body.message;
     const subject = req.body.subject; 
 
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) == false) {
+        res.status(500).end(JSON.stringify({ message: 'please fill validate email!' }))
+        return ;
+    }
+
+    if(!process.env.Password) {
+        alert('sorry for inconvience ! Currently portforlio is runnion on localhost so your message can\'t reach to developer . thanks for trying to reach out.  ')
+    }
+
     const client = new SMTPClient({
         user: process.env.Username,
         password: process.env.Password,
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -38,10 +38,31 @@ export default function Contact() {
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
   const [subject, setSubject] = useState('')
+
+  const changeValue = (field: any, value: any) => {
+
+    if (!process.env.Password) {
+      alert('sorry for inconvience ! Currently portforlio is runnion on localhost so your message can\'t reach to developer . thanks for trying to reaching me out. You can connect me by clicking following social icons. ')
+      return 
+    }
+    field == 'email' ?
+      setEmail(value)
+      : field == 'sub' ?
+        setSubject(value)
+        : setMessage(value)
+  }
+
   const validateFormWithJS = (e: any) => {
     e.preventDefault();
-    const body = {email , message, subject}
-    axios.post('http://localhost:3000/api/email', { email,message,subject })
+    var mail_container = document.getElementById('email')
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) == false) {
+      alert('request kindly fill out a valid email address !')
+      mail_container.style = "borderColor:red"
+      mail_container?.focus();
+      return
+    }
+
+    axios.post('http://localhost:3000/api/email', { email, message, subject })
       .then(
 
         (res) => {
@@ -75,15 +96,18 @@ export default function Contact() {
             Feel free to fill out the contact form , and I will respond to you as soon as possible.
           </p>
           <form className={contactStyles.form_column}>
-            <input type="text" name="email" id="email" placeholder='your email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="text" name="email" id="email" className="input" placeholder='your email'
+              value={email} onChange={(e) => changeValue('email', e.target.value)}
+              required />
 
-            <input type="text" name="subject" id="subject" placeholder='subject' value={subject} onChange={(e) => setSubject(e.target.value)} required />
+            <input type="text" name="subject" id="subject" className="input" placeholder='subject' value={subject} onChange={(e) => changeValue('sub', e.target.value)} required />
 
             <textarea rows={5}
               placeholder="Message Me ... "
-              onChange={(e) => setMessage(e.target.value)}
+              onChange={(e) => changeValue('msg', e.target.value)}
               name="message"
               value={message}
+              className="input"
               id="message" />
 
             <button type="submit" onClick={validateFormWithJS}>Submit</button>
@@ -93,7 +117,7 @@ export default function Contact() {
         <div className="explanation">
 
           <p>
-            Alternatively, you can also connect with me through the social media icons provided below. Feel free to follow me on Twitter, or connect with me on LinkedIn. I'm active on these platforms.
+            Alternatively, you can also connect with me through the social media icons provided below. Feel free to connect with me on LinkedIn. I'm active on these following platforms.
 
             Thank you for visiting my portfolio, and I look forward to hearing from you soon. Let's create something amazing together!
           </p>
